751d6325c38201f1c91df2e624f20b1b05bccb11
IgorBeloglazov <79027637+IgorBeloglazov@users.noreply.github.com>
2021-02-16 20:57:47 +0200

    Add files via upload

--- Lesson_1/HelloWorld.java
+++ Lesson_1/HelloWorld.java
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 public class HelloWorld {
     public static void main (String [] args) {
 
         System.out.println("Hello World!");
     }
+=======
+public class HelloWorld {
+    public static void main (String [] args) {
+
+        System.out.println("Hello World!");
+    }
+>>>>>>> d2e6192fc3c148814c4a539d8dbba41057a78530
 }
